f3c5f5e019fa23382727fe9cf55249bb66e3f1e7
Pickachu Chen <pickachu1006@gmail.com>
2020-12-25 14:57:02 +0800

    20201225_offer add btn

--- js/all.js
+++ js/all.js
@@ -5,17 +5,10 @@ $(document).ready(function () {
     });
     observer.observe();
     // 立即購買btn動態
-    $('.offerbtn, .offerbtn_m').click(function (e) { 
+    $('.allset,a[href="#buy"]').click(function (e) { 
         e.preventDefault();
         $('html , body').animate({
-            scrollTop: $('#size').offset().top,
-        },900);
-    });
-    // 立即訂購btn動態
-    $('.buybtn,a[href="#size"]').click(function (e) { 
-        e.preventDefault();
-        $('html , body').animate({
-            scrollTop: $('#size').offset().top,
+            scrollTop: $('#buy').offset().top,
         },900);
     });
 
@@ -27,10 +20,4 @@ $(document).ready(function () {
         $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
     }
     
-    //週年慶活動辦法下拉選單
-    // $(".anniversary_2").hide();
-    // $('.anniversary_1').click(function (e) { 
-    //     e.preventDefault();
-    //     $(".anniversary_2").toggle();
-    // });
 });
